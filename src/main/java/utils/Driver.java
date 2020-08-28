4e5efd49a8713f15c74882e2a956a68093d66e1d
Kumar Shubham <kumar.shubham@blackbuck.com>
2020-08-28 20:42:36 +0530

    Added Driver class

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -1,4 +1,28 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Driver {
+
+    public static WebDriver webDriver;
+    public static WebDriver getWebDriverInstance(){
+        try {
+            if(webDriver==null && Constants.BROWSER.equalsIgnoreCase("CHROME")) {
+                System.setProperty("webdriver.chrome.driver", "DriverFiles/chromedriver");
+                webDriver = new ChromeDriver();
+            }
+            else if (webDriver==null && Constants.BROWSER.equalsIgnoreCase("FIREFOX")){
+                System.setProperty("webdriver.gecko.driver", "DriverFiles/geckodriver");
+                webDriver = new FirefoxDriver();
+
+            }
+            else{
+                return webDriver;
+            }
+        }catch(Exception e){
+        }
+        return webDriver;
+    }
 }
